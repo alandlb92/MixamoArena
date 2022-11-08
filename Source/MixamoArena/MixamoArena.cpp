9ffbf4ff139e1c4d8d265ce40363cfdabb4d2ec8
Alan <alan.dlucas.bittencourt@gmail.com>
2022-11-07 21:34:54 -0300

    first commit

---
+++ Source/MixamoArena/MixamoArena.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MixamoArena.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MixamoArena, "MixamoArena" );
---
+++ Source/MixamoArena/MixamoArenaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MixamoArenaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIXAMOARENA_API AMixamoArenaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
