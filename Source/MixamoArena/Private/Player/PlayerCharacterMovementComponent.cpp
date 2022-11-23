a731d90746c640c21cb328521b5a16846715654b
Alan <alan.dlucas.bittencourt@gmail.com>
2022-11-22 21:07:17 -0300

    Update movement controller

--- Source/MixamoArena/Private/Player/PlayerCharacterMovementComponent.cpp
+++ Source/MixamoArena/Private/Player/PlayerCharacterMovementComponent.cpp
@@ -4,7 +4,19 @@
 #include "Player/PlayerCharacterMovementComponent.h"
 #include "Player/PlayerAnimInstance.h"
 
+void UPlayerCharacterMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
 
+void UPlayerCharacterMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	FVector2D input = _inputInfo.GetThumbstickLeft();
+	UE_LOG(LogTemp, Warning, TEXT("Input x: %f y: %f"), input.X, input.Y);
+	_inputInfo.Clean();
+	UE_LOG(LogTemp, Warning, TEXT("CLEANED AWAYS 0, 0 Input x: %f y: %f"), input.X, input.Y);
+}
 
 void UPlayerCharacterMovementComponent::Configure(UPlayerAnimInstance* anim)
 {
@@ -16,7 +28,8 @@ void UPlayerCharacterMovementComponent::MoveHorizontal(float input)
 	if (input != 0)
 		UE_LOG(LogTemp, Warning, TEXT("MoveHorizontal %f"), input);
 
-	_anim->_horizontalInput = input;
+	_inputInfo.SetThumbstickLeftX(input);
+	//_anim->_horizontalInput = input;
 }
 
 
@@ -25,5 +38,6 @@ void UPlayerCharacterMovementComponent::MoveVertical(float input)
 	if (input != 0)
 		UE_LOG(LogTemp, Warning, TEXT("MoveVertical %f"), input);
 
-	_anim->_verticalInput = input;
+	_inputInfo.SetThumbstickLeftY(input);
+	//_anim->_verticalInput = input;
 }
--- Source/MixamoArena/Public/Player/PlayerCharacterMovementComponent.h
+++ Source/MixamoArena/Public/Player/PlayerCharacterMovementComponent.h
@@ -4,18 +4,53 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Math/Vector2D.h"
 #include "PlayerCharacterMovementComponent.generated.h"
 
-/**
- * 
- */
+USTRUCT()
+struct MIXAMOARENA_API FInputInfo
+{
+	GENERATED_BODY()
+private:
+	float _inputX;
+	float _inputY;
+
+public:
+	void Clean()
+	{
+		_inputX = 0;
+		_inputY = 0;
+	}
+
+	void SetThumbstickLeftX(float input) 
+	{
+		_inputX = input;
+	}
+	void SetThumbstickLeftY(float input)
+	{
+		_inputY = input;
+	}
+
+	FVector2D GetThumbstickLeft()
+	{
+		return FVector2D(_inputX, _inputY);
+	}
+};
+
 UCLASS()
 class MIXAMOARENA_API UPlayerCharacterMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
 
 private:
+	UPROPERTY()
 	class UPlayerAnimInstance* _anim;
+	UPROPERTY()
+	FInputInfo _inputInfo;
+
+protected:
+	void BeginPlay() override;
+	void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 public:
 	void Configure(UPlayerAnimInstance* anim);
